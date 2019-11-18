ed82017e0022bf52d6680071f7ebf59f75543284
Oksana Nichols <oksanahnichols@yahoo.com>
2019-11-18 07:23:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,3 +4,12 @@
 def key_for_min_value(name_hash)
 
 end
+describe "smallest hash value" do
+
+  it "does not call the `#keys` method" do
+    hash = {:blake => 500, :ashley => 2, :adam => 1}
+
+    expect(hash).to_not receive(:keys)
+
+    key_for_min_value(hash)
+  end
